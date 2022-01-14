4bf53cf7359549f1612c0eee976e06bf92df5c03
udobnaja <udobnaja@yandex-team.ru>
2022-01-14 18:37:49 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anna Udobnaja on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
